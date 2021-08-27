23215fa1751a76fc0e05bd574c1a374f32545e27
Karthik <karthik.1rn16ei020@gmail.com>
2021-08-27 20:52:56 +0530

    first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,45 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavourite }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsIsVisible">
+      <li>{{ email }}</li>
+      <li>{{ phone }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+
+  data() {
+    return {
+      detailsIsVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsIsVisible = !this.detailsIsVisible;
+    },
+  },
+};
+</script>
